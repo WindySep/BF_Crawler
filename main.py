2e19af06bf5792b3969a4446ec0b99a907972600
WindySep <ruoyanghe@qq.com>
2023-10-09 09:47:28 +0800

    更改输入方式

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 from crawler import Crawler
 
 # 输入目标的UID，作为一个字符串
-# uid = str(input("输入查询用户的UID："))
-uid = '2070667222'
+uid = str(input("输入查询用户的UID："))
+# uid = '2070667222'
 
 Crawler.crawler(uid)
